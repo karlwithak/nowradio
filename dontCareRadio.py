69423887a2e65f93a705ee6b6e677c56bcc3f9f1
Nicholas Hrynuik <nhrynuik@gmail.com>
2015-02-07 21:35:57 -0500

    Added bootstrap. Created the landing page. Set up the static file directory.

--- dontCareRadio.py
+++ dontCareRadio.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+# This file is the first that is called when someone goes to a page on out website.
+# It looks at the url that they entered and decided what computation to do and what
+#   template to show them.
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("landing.html")
 
 
 if __name__ == '__main__':
