d82991cf484d728054abf218898912aa177ad5b3
Nicholas Hrynuik <nhrynuik@gmail.com>
2015-02-19 21:47:16 -0500

    Make updateIpAddr.py multithreaded. Included port number in ip address (doh!)

--- ourUtils.py
+++ ourUtils.py
@@ -3,6 +3,8 @@ import socket
 
 def ip_from_url(url):
     url = url.replace("http://", "")[:-1]
+    port = ""
     if ":" in url:
+        port = url[url.index(":"):]
         url = url[:url.index(":")]
-    return socket.gethostbyname(url)
+    return socket.gethostbyname(url) + port
--- updateIpAddr.py
+++ updateIpAddr.py
@@ -2,13 +2,11 @@ from dbManager import Queries, dbpass
 import psycopg2
 import socket
 import ourUtils
+import threading
 
 
-def main():
-    conn = psycopg2.connect("dbname=radiodb user=radiodb host=localhost password=%s" % dbpass)
+def worker(id_url_list, conn):
     cur = conn.cursor()
-    cur.execute(Queries.get_all_urls)
-    id_url_list = cur.fetchall()
     for id_url in id_url_list:
         try:
             ip = ourUtils.ip_from_url(id_url[1])
@@ -17,6 +15,23 @@ def main():
         else:
             cur.execute(Queries.set_ip_for_id, [ip, id_url[0]])
     cur.close()
+
+
+def main():
+    conn = psycopg2.connect("dbname=radiodb user=radiodb host=localhost password=%s" % dbpass)
+    main_cur = conn.cursor()
+    main_cur.execute(Queries.get_all_urls)
+    id_url_list = main_cur.fetchall()
+    threads = []
+    thread_count = 25
+    step = len(id_url_list)/thread_count
+    for i in range(0, len(id_url_list), step):
+        t = threading.Thread(target=worker, args=(id_url_list[i:i + step], conn))
+        threads.append(t)
+        t.start()
+    for thread in threads:
+        thread.join()
+    main_cur.close()
     conn.commit()
     conn.close()
 
