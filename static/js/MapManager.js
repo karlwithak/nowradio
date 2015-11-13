9103183a7f1a4e4e0b11a4ca0110ce412b1811f6
Victor Szeto <me@victorszeto.com>
2015-11-13 18:27:12 -0500

    Add comment, set initial zoom to 3

--- static/js/MapManager.js
+++ static/js/MapManager.js
@@ -1,12 +1,16 @@
 /* global $ */
 
-NowRadio.MapManager = (function (nr) {
+/**
+ * Manage the map and pins on the map
+ */
+var NowRadio = (function (nr) {
+    'use strict';
     var MapManager = {};
     var style = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
     window.initMap = MapManager.initMap = function () {
         var map = new google.maps.Map(document.querySelector('.verticalCenter'), {
             center: {lat: 37.9481, lng: -119.7881},
-            zoom: 10,
+            zoom: 3,
             disableDefaultUI: true,
             styles: style
         });
