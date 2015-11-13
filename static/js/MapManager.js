d4f16f9df5848b73658edcc66667a48873e9142f
Victor Szeto <me@victorszeto.com>
2015-11-13 18:31:33 -0500

    Change module pattern

--- static/js/MapManager.js
+++ static/js/MapManager.js
@@ -5,9 +5,9 @@
  */
 var NowRadio = (function (nr) {
     'use strict';
-    var MapManager = {};
+    nr.MapManager = {};
     var style = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
-    window.initMap = MapManager.initMap = function () {
+    window.initMap = nr.MapManager.initMap = function () {
         var map = new google.maps.Map(document.querySelector('.verticalCenter'), {
             center: {lat: 37.9481, lng: -119.7881},
             zoom: 3,
@@ -18,8 +18,9 @@ var NowRadio = (function (nr) {
         google.maps.event.addListenerOnce(map, 'idle', function() {
                 google.maps.event.trigger(map, 'resize');
         });
-        NowRadio.MapManager.map = map;
+
+        nr.MapManager.map = map;
     }
-    return MapManager;
+    return nr;
 }(NowRadio || {}));
 
